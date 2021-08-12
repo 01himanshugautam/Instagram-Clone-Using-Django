cf7a7c2a35c2f93e7666b7ea516698393a3db6bf
Himanshu Gautam <01himanshugautam@gmail.com>
2021-08-12 22:11:39 +0530

    addd file structure

--- instagram_clone_using_django/settings.py
+++ instagram_clone_using_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instagram.apps.InstagramConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'instagram_clone_using_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['instagram/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
