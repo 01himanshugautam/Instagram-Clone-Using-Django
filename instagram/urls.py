3aa7fb5012cc05f5fb33d21c164c4192a0a17356
Himanshu Gautam <01himanshugautam@gmail.com>
2021-08-12 22:23:15 +0530

    addd file structure

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login/", views.login, name="login"),
+    path("signUp/", views.signup, name="signUp"),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
